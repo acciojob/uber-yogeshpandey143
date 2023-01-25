3fd40a4f9620dd27f76b0c1dd16a871a5be7cc28
Yogesh <bc21143@smsvaranasi.in>
2023-01-25 22:55:30 +0530

    new commit

--- src/main/java/com/driver/controllers/DriverController.java
+++ src/main/java/com/driver/controllers/DriverController.java
@@ -11,25 +11,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/driver")
 public class DriverController {
 	
-	
-	@Autowired DriverService driverService; 
-	
 	@PostMapping(value = "/register")
 	public ResponseEntity<Void> registerDriver(@RequestParam String mobile, @RequestParam String password){
-		
-		driverService.register(mobile, password);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
 	@DeleteMapping(value = "/delete")
 	public void deleteDriver(@RequestParam Integer driverId){
-		
-		driverService.removeDriver(driverId);
 	}
 
 	@PutMapping("/status")
 	public void updateStatus(@RequestParam Integer driverId){
-		
-		driverService.updateStatus(driverId);
 	}
 }
